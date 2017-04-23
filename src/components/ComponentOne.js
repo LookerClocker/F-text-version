5d37217a955b414d0e1e2d40feb56b63e0ec56cd
Olexandr Kushnir <olexandrrazor@mail.ru>
2017-04-23 19:23:05 +0300

    added some files

---
+++ src/components/ComponentOne.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Main extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello, world
+            </div>
+        )
+    }
+}
---
+++ src/components/ComponentTwo.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default class MainIndex extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello, world Index
+            </div>
+        )
+    }
+}
