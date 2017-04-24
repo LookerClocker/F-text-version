84002490fe9d42fffce553bd9e8a03dab5f462b6
Olexandr Kushnir <olexandrrazor@mail.ru>
2017-04-24 23:23:06 +0300

    fixed path to Link component

--- src/components/ComponentOne.js
+++ src/components/ComponentOne.js
@@ -1,12 +1,13 @@
 
 import React, { Component } from 'react';
-
+import { Link }  from 'react-router-dom';
 
 export default class ComponentOne extends Component {
     render() {
         return(
             <div>
                 Hello, world
+                <Link to="/two">Link</Link>
             </div>
         )
     }
