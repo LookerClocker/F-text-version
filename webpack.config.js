4727ae8e52961441cc1b7086fc7ce452a27c0590
LookerClocker <sahakushnir@gmail.com>
2017-04-26 18:13:38 +0300

    Trying to manage SSR

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const path = require('path');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
 
     entry: path.join(__dirname, 'src', 'app-client.js'),
@@ -39,23 +41,53 @@ module.exports = {
     },
 
     module: {
-        rules: [{
-            test: path.join(__dirname, 'src'),
-            // test: /\.(js|jsx)$/,
-            loader: 'babel-loader',
-            query: {
-                presets: debug ? ['react', 'es2015', 'react-hmre'] : ['react', 'es2015']
+        rules: [
+            {
+                test: path.join(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: debug ? ['react', 'es2015', 'react-hmre'] : ['react', 'es2015']
+                }
+            },
+
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
             }
-        }]
+
+
+            // { test:/\.css$/, use: ['isomorphic-style-loader','style-loader','css-loader']}
+
+            // {
+            //     test: /\.scss$/,
+            //     use: ExtractTextPlugin.extract({
+            //     fallback: 'style-loader',
+            //     use: ['css-loader','sass-loader']
+            // })
+            // }
+        ]
     },
 
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            // 'process.env': {
+            //   BROWSER: JSON.stringify(true)
+            // }
         }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'common'
-        // }),
+
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: 1
+        }),
         new webpack.optimize.UglifyJsPlugin({
             unsafe: true,
             drop_console: true
